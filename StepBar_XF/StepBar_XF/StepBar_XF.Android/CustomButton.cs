d1d6b66a9657fe66fedccd80cde7249f30054a53
Elif Aybike Aslan <elfaybke@gmail.com>
2018-09-15 12:05:08 +0300

    StepProgressBarControl

---
+++ StepBar_XF/StepBar_XF/StepBar_XF.Android/CustomButton.cs
@@ -0,0 +1,21 @@
+﻿
+using StepBar_XF.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Button), typeof(CustomButton))]
+namespace StepBar_XF.Droid
+{
+    public class CustomButton : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+
+            }
+        }
+    }
+}
